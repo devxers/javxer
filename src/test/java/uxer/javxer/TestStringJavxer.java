e132285ea49dd91617424f4ec5c63405e406b701
GabrielDS <gabriel.santosti01@gmail.com>
2017-12-25 11:31:12 -0300

    define init tests of the StringJavxer class

--- src/test/java/uxer/javxer/TestStringJavxer.java
+++ src/test/java/uxer/javxer/TestStringJavxer.java
@@ -5,4 +5,36 @@ import static org.junit.Assert.*
 
 public class TestStringJavxer {
 
+    // SetUp
+    private StringJavxer javxer;
+
+    //Test capitalizeFirstLetter
+    private String resultTestFirstUppercase = "Diego";
+    private String stringTestFirstUppercase = "diego";
+
+    // Test abbreviateName
+    private int max_length = 9;
+    private String resultAbbreviateName = "Kevin S. G.";
+    private String stringAbbreviateName = "Kevin smith garcias";
+    private String stringLowerCaseAbbreviateName = "kevin smith garcias";
+
+    @Before
+    public void setUp() {
+        javxer = new StringJavxer();
+    }
+
+    @Test
+    public void testCapitalizeFirstLetter() {
+        assertEquals( resultTestFirstUppercase, javxer.capitalizeFirstLetter( stringTestFirstUppercase ) );
+    }
+
+    @Test
+    public void testAbbreviateName() {
+        assertEquals( resultAbbreviateName, javxer.abbreviateName( stringAbbreviateName, max_length ) );
+    }
+
+    @Test
+    public void testAbbreviateNameWithAllLowerCase() {
+        assertEquals( resultAbbreviateName, javxer.abbreviateName( stringLowerCaseAbbreviateName, max_length ) );
+    }
 }

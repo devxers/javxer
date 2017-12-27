69dcadaeef2a4370ef0fae14c55773f7788a6e3c
GabrielDS <gabriel.santosti01@gmail.com>
2017-12-26 23:14:57 -0300

    add a comma on the regex to invalid emails that start with comma

--- src/main/java/upxer/javxer/UtilJavxer.java
+++ src/main/java/upxer/javxer/UtilJavxer.java
@@ -9,6 +9,6 @@ public class UtilJavxer {
     }
 
     public boolean ifIsAnEmail( String text ) {
-        return text.matches( "^[^\\.][\\w+!]+@(?:[A-z0-9]+\\.)+[A-z]{1,6}$" );
+        return text.matches( "^[^\\.,,][\\w+!]+@(?:[A-z0-9]+\\.)+[A-z]{1,6}$" );
     }
 }
